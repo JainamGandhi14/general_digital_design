08dddb48da31d55fbbddbe644bc98b81f28989df
JainamGandhi14 <jainamgandhi1997@gmail.com>
2025-02-27 23:26:28 -0500

    Update interrupt_control_priority_encode.v - final

--- interrupt_control_priority_encode.v
+++ interrupt_control_priority_encode.v
@@ -1,17 +1,17 @@
 `timescale 1ns / 1ps
-module interrupt_control_priority_encode #(parameter No_IP = 4)(
+module #(parameter NINTR = 4)interrupt_control_priority_encode(
   input        clk,
   input        reset_n,
-  input  [No_IP-1:0] req,
+  input  [NINTR-1:0] req,
   input        done,
-  output reg [No_IP-1:0] ack,
+  output reg [NINTR-1:0] ack,
   output reg       irq
     );
-  
-  parameter bit_req = $clog2(No_IP);
+  //NINTR is number of interrupts
+  parameter bit_req = $clog2(NINTR);
   wire [bit_req-1:0] code;
 wire valid;
-  reg [No_IP-1:0] prev_req; //will be needed to process the previous requests when waiting if no high priority new request is asserted
+  reg [NINTR-1:0] prev_req; //will be needed to process the previous requests when waiting if no high priority new request is asserted
 reg [2:0] state; //using one-hot encoding
 localparam IDLE = 3'b001,
            ACK = 3'b010,
@@ -19,90 +19,81 @@ localparam IDLE = 3'b001,
 priority_encoder encode(.enable(1'b1),.req(prev_req),.code(code),.valid(valid));
 
 
-//prev req -- because of using 1 extra reg to capture and manipulate input, some input req depending 
-//on the current state, if current state == ACK, then the captured latency is 1 clock cyc;e
 
-always@(posedge clk, negedge reset_n)
-begin
-    if(~reset_n)    begin
-        prev_req <= 0;
-    end
-    else    begin
-        if(state==IDLE && req !=0)  begin
-            prev_req <= prev_req | req;
-        end
-        else if(state==ACK)   begin
-            prev_req[code] <= 1'b0;  //removing previously assigned priority 
-        end
-        else if(state==WAIT_DONE)   begin
-            prev_req <= prev_req | req; // masking new req
-        end
-    end
-end
-
-//ack op && irq
-always@(posedge clk, negedge reset_n)
-begin
-    if(~reset_n)    begin
-        ack <= 0;
-    end
-    else    begin
-        if(state==IDLE)  begin
-            ack <= 0;
-            irq <= 0;
-        end
-        else if(state==ACK)   begin
-            ack <= 1'b1 << code; 
-            irq <= valid;
-        end
-        else if(state==WAIT_DONE)   begin
-            ack<=0;
-            irq<=0;
-        end
-    end
-end
-
-//state machine
+//state machine and input manipulation
 always@(posedge clk, negedge reset_n)
 begin
     if(~reset_n)    begin
         state <= IDLE;
+        prev_req <= 0;
     end
     else    begin
         case(state)
         IDLE:   begin
-            if(prev_req != 0)    begin
+            if(prev_req || req) begin
                 state <= ACK;
+                prev_req <= prev_req | req;
             end
-            end
+        end
         ACK:    begin
                 state <= WAIT_DONE;
-            end
+                prev_req <= (prev_req & (~ack)) | req;
+        end
         WAIT_DONE:  begin
-                if(done)    begin
-                    state <= IDLE;//if done, go to idle state
-                end
-                else if(prev_req!=0)   begin
-                  state <= ACK;//if req are pending and not rcvd done yet then go to ACk state to process next pending req
-                end
+            if(done)    begin
+                state <= IDLE;
+                prev_req <= prev_req | req;
             end
+            else if(prev_req!=0)    begin
+                state <= ACK;
+                prev_req <= prev_req | req;
+            end
+        end
         default:    begin
-                    state <= IDLE;
         end
         endcase
+    end
+end
+//output mech
+always@(posedge clk, negedge reset_n)
+begin
+    if(~reset_n)    begin
+        ack <= 0;
+        irq <= 0;
+    end
+    else    begin
+        if(state == IDLE && (prev_req || req))   begin
+            ack <= 1'b1 << code;
+            irq <= valid;
+        end
+        else if(state==ACK) begin
+            ack <= ack;
+            irq <= irq;
+        end
+        else if(state==WAIT_DONE)   begin
+            if(done)    begin
+                ack <= 0;
+                irq <= 0;
+            end
+            else if(prev_req !=0)   begin
+                ack <= 1'b1 << code;
+                irq <= valid;
+            end
+        end
     end
 end
 
 endmodule
 
-module priority_encoder #(parameter No_IP = 4)(
+module priority_encoder #(parameter NINTR = 4)(
   input        enable,
-  input  [No_IP-1:0] req,
+  input  [NINTR-1:0] req,
   output reg [bit_req-1:0] code,
   output       valid
 );
-  parameter bit_req = $clog2(No_IP);
-reg [2:0] i; //to maintain the priority - easy to scale
+
+  parameter bit_req = $clog2(NINTR);
+  reg [bit_req:0] i; //to maintain the priority - easy to scale
 assign valid = (req != 0 ) && enable;
 
 always@(*)
@@ -112,7 +103,7 @@ begin
     end
     else    begin
         code = 0;
-      for(i=0;i<No_IP;i=i+1)   begin : WILL_GEN_COMB
+      for(i=0;i<NINTR;i=i+1)   begin : WILL_GEN_COMB
             code = req[i]? i[1:0] : code;
         end
     end
