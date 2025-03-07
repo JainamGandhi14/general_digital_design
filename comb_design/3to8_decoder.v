2900f5e3c261f398ce62a732e25a0676286592a7
JainamGandhi14 <jainamgandhi1997@gmail.com>
2025-03-07 18:23:33 -0500

    Create 3to8_decoder.v

---
+++ comb_design/3to8_decoder.v
@@ -0,0 +1,9 @@
+`timescale 1ns / 1ps
+module decoder_3to8 (
+    input [2:0] sel,   // 3-bit select input
+    output /*reg*/ [7:0] out  // 8-bit output
+); //a decoder without any enable signal
+//elegant solution and very easy to scale by changing IO bits only.
+assign out = 1'b1<<sel; 
+
+endmodule
