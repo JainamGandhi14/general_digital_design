fd408be6bf3ca4dcbfcae291a07db59bc27b3be9
JainamGandhi14 <jainamgandhi1997@gmail.com>
2025-03-07 18:21:56 -0500

    Create mux_4to1.v

---
+++ comb_design/mux_4to1.v
@@ -0,0 +1,21 @@
+module mux_4to1 (
+    input [3:0] A,    // 4-bit input A
+    input [3:0] B,    // 4-bit input B
+    input [3:0] C,    // 4-bit input C
+    input [3:0] D,    // 4-bit input D
+    input [1:0] sel,  // 2-bit select signal
+    output reg [3:0] out // 4-bit output
+);
+
+always@(*)
+begin: MUX
+case(sel)
+2'b00 : out = A;
+2'b01 : out = B;
+2'b10 : out = C;
+2'b11 : out = D;
+default:    out = 4'b0;
+endcase
+end
+
+endmodule
