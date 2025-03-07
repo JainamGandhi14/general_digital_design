6751bc5dbedf9f084eebbc09f31be5f599849812
JainamGandhi14 <jainamgandhi1997@gmail.com>
2025-03-07 18:24:36 -0500

    Update binary_counter_moore.v

--- fsm_designs/binary_counter_moore.v
+++ fsm_designs/binary_counter_moore.v
@@ -1,25 +1,5 @@
 `timescale 1ns / 1ps
 
-/*module leading_zero_counter #(parameter len = 4)(
-    input [len-1:0] a,
-    output reg [$clog2(len):0] z
-    );
-    integer i;
-    reg flag;
-    always@(*)
-    begin
-        flag = 0;
-        z = len;
-        for(i=len-1;i>=0;i=i-1)   begin
-            if(a[i] && (!flag))    begin
-                z = len - 1 - i;
-                flag = 1;
-            end
-        end
-    end
-    
-endmodule
-*/
 module counter_fsm(
     input clk,          // Clock signal
     input rst,          // Reset signal
